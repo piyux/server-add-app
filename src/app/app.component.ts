6dfc656cfa82a13e0a8f521355faf279c107e07b
Piyush <piyuinshort@gmail.com>
2021-01-21 17:35:10 +0530

    Buttons Enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ export class AppComponent {
   serverElements = [ {type: 'server',name: 'Test' ,content: 'This is First Web Server'} ,
   {type: 'server' , name: 'Test 2' , content: 'This is Second Web Server'}];
   title = 'server-add-app';
+  onServerAdded(serverData: {serverName: string,serverContent: string}){
+    this.serverElements.push(
+      {
+        type: 'server',
+        name: serverData.serverName,
+        content: serverData.serverContent
+      }
+    );
+  }
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   constructor() { }
   newServerName = '';
   newServerContent ='';
   ngOnInit(): void {
   }
-  onAddServer(){}
-  onAddBlueprint(){}
+  onAddServer(){
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+  onAddBlueprint(){
+    this.bluePrintCreated.emit({
+      serverName: 'SAMPLE',
+      serverContent: 'LOREM TOTEM'
+    })
+  }
 }
