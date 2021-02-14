82c6eb2b848bfb52cd4f4c0a65eaac29a628c783
Piyush <piyuinshort@gmail.com>
2021-02-14 09:42:59 +0530

    added @ViewChild()

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { isLoweredSymbol } from '@angular/compiler';
+import { Component, ElementRef, EventEmitter, OnInit, Output ,ViewChild} from '@angular/core';
+import { ServerElementComponent } from '../server-element/server-element.component';
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
@@ -9,20 +11,21 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   constructor() { }
   newServerName = '';
-  newServerContent ='';
+  //newServerContent ='';
+  @ViewChild('serverContentInput' ,{static:true}) serverContentInput: ElementRef;
   ngOnInit(): void {
   }
   onAddServer(){
     this.serverCreated.emit({
       serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
   onAddBlueprint(){
     //console.log('debug blueprint');
     this.bluePrintCreated.emit({
       serverName: 'SAMPLE',
-      serverContent: 'LOREM TOTEM'
+      serverContent: 'This is sample web server'
     });
     //console.log(this)
   }
